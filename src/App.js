2ffbd3479bcbbc932e50e64818b58327319d4ec9
Muhammad Asif Javed <axif.javed@gmail.com>
2019-05-30 13:30:47 +0500

    ant design implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>Digital Signage Auth Form Goes !!</h1>
-    </div>
-  );
+import { Menu, Icon } from 'antd';
+
+const { SubMenu } = Menu;
+
+class App extends React.Component {
+  state = {
+    current: 'mail',
+  };
+
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  };
+
+  render() {
+    return (
+      <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+        <Menu.Item key="login">
+          <Icon type="login" />
+          Login
+        </Menu.Item>
+        <Menu.Item key="register">
+          <Icon type="user" />
+          Register
+        </Menu.Item>
+      </Menu>
+    );
+  }
 }
 
 export default App;
