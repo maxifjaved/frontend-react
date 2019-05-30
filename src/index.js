ac6d82ccf2a12d66b316db21ba7b67e998164b01
Muhammad Asif Javed <axif.javed@gmail.com>
2019-05-30 11:20:03 +0500

    redux setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './assets/index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 
+import * as serviceWorker from './serviceWorker'
+import App from './App'
+import configureStore from './configureStore'
+
+
+const store = configureStore();
+
+const renderApp = () =>
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
+
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept('./App', renderApp)
+}
+
+renderApp()
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
